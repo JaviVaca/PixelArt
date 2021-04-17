715c2a9875a1d8429fa1da4a2c0228904c4ad86a
epm <epm@shkways.com>
2021-04-17 14:39:54 +0200

    ya esta el color seleccionado, el pintar o borrar, falta que pinte todos los que vaya tocando al arrastrar el dedo por la pantalla. Ya se pinta el que se clicka

--- app/src/main/java/com/example/pixelart/AdaptadorGrid.java
+++ app/src/main/java/com/example/pixelart/AdaptadorGrid.java
@@ -1,14 +1,24 @@
 package com.example.pixelart;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.DragEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.core.content.res.ResourcesCompat;
 
@@ -61,6 +71,7 @@ public class AdaptadorGrid extends BaseAdapter {
         }
         casilla.setLinea(linea);
 
+        tvGrid.setTag(position+"/"+linea);
         if (linea%2==0) {
             if (casilla.getColor()==0) {
                 tvGrid.setBackgroundColor(Color.WHITE);
@@ -74,6 +85,70 @@ public class AdaptadorGrid extends BaseAdapter {
             else {tvGrid.setBackgroundColor(Color.WHITE);}
         }
 
+
+        View finalView = view;
+        tvGrid.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                Context ctx = finalView.getContext();
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    putPref(ctx.getString(R.string.lineaSeleccionada), String.valueOf(position), ctx.getApplicationContext());
+                    putPref(ctx.getString(R.string.cuadradoSeleccionado), String.valueOf((position/casilla.getLinea())-1), ctx.getApplicationContext());
+                }
+                if(event.getAction() ==MotionEvent.ACTION_MOVE){
+                    putPref(ctx.getString(R.string.lineaSeleccionada), String.valueOf(position), ctx.getApplicationContext());
+                    putPref(ctx.getString(R.string.cuadradoSeleccionado), String.valueOf((position/casilla.getLinea())-1), ctx.getApplicationContext());
+                }
+                if(event.getAction() ==MotionEvent.ACTION_UP){
+                    putPref(ctx.getString(R.string.lineaSeleccionada), String.valueOf(position), ctx.getApplicationContext());
+                    putPref(ctx.getString(R.string.cuadradoSeleccionado), String.valueOf((position/casilla.getLinea())-1), ctx.getApplicationContext());
+                }
+                return true;
+            }
+        });
+//        LinearLayout padre =(LinearLayout)tvGrid.getParent();
+//        GridView padreGridLayout ;
+//        LinearLayout padrePadre =(LinearLayout)padre.getParent();
+//
+//        padreGridLayout = (GridView) padrePadre.getParent();
+//        padreGridLayout.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//
+//                if ((event.getAction() == MotionEvent.ACTION_DOWN)||(event.getAction() ==MotionEvent.ACTION_MOVE)) {
+//
+//                    Context ctx = finalView.getContext();
+//
+//                    String valorSeleccionado = getPref(ctx.getString(R.string.seleccionado), ctx);
+//
+//                    //en caso de que se haya seleccionado el boton de pintar, se pintara del color seleccionado
+//                    //anteriormente en la paleta
+//                    if (valorSeleccionado.equalsIgnoreCase(ctx.getString(R.string.pintar))) {
+//                        String colorSeleccionadoShared = getPref(ctx.getString(R.string.colorSeleccionado), ctx);
+//                        Toast.makeText(finalView.getContext(), "color seleccionado grid->" + colorSeleccionadoShared, Toast.LENGTH_SHORT).show();
+//                        GridLayout parent=(GridLayout)v;
+//                        View vistaHija= parent.getChildAt(position);
+//                        vistaHija.setBackgroundColor(Integer.parseInt(colorSeleccionadoShared));
+//                    } else if (valorSeleccionado.equalsIgnoreCase(ctx.getString(R.string.borrar))) {
+//
+//                    }
+//                }
+//                return false;
+//            }
+//        });
+
         return view;
+    }
+    public static String getPref(String key, Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(key, null);
+    }
+    public static void putPref(String key, String value, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(key, value);
+        editor.apply();
     }
 }
--- app/src/main/java/com/example/pixelart/MainActivity.java
+++ app/src/main/java/com/example/pixelart/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.pixelart;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Lifecycle;
 
+import android.app.usage.UsageEvents;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.EventLog;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.GridLayout;
 import android.widget.GridView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
     private GridView gridView;
+    private GridLayout paleta;
     private AdaptadorGrid adaptadorGrid;
     private FloatingActionButton fabPintar, fabBorrar, fabNuevo;
     private Boolean pintar=true, borrar=false;
@@ -26,20 +42,103 @@ public class MainActivity extends AppCompatActivity {
         adaptadorGrid = new AdaptadorGrid(this);
         gridView.setAdapter(adaptadorGrid);
 
+//        LinearLayout padre =(LinearLayout)tvGrid.getParent();
+//        GridView padreGridLayout ;
+//        LinearLayout padrePadre =(LinearLayout)padre.getParent();
+//
+//        padreGridLayout = (GridView) padrePadre.getParent();
+        gridView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+
+                    pintarBorrar(v,event);
+                }
+                if(event.getAction() ==MotionEvent.ACTION_MOVE){
+                    pintarBorrar(v,event);
+                }
+                if(event.getAction() ==MotionEvent.ACTION_UP){
+                    pintarBorrar(v,event);
+                }
+                return true;
+            }
+        });
+
+        paleta= findViewById(R.id.paleta);
         fabPintar= findViewById(R.id.fabPintar);
         fabBorrar= findViewById(R.id.fabBorrar);
         fabNuevo = findViewById(R.id.fabNuevo);
 
+        for (int i =0;i<paleta.getChildCount();i++){
+            paleta.getChildAt(i).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Drawable colorSeleccionadoBck;
+                    colorSeleccionadoBck =v.getBackground();
+                    int colorSeleccionado=-1;
+                    if (colorSeleccionadoBck instanceof ColorDrawable) {
+                        colorSeleccionado = ((ColorDrawable) colorSeleccionadoBck).getColor();
+                    }
+
+                    putPref(getString(R.string.colorSeleccionado), String.valueOf(colorSeleccionado), getApplicationContext());
+                    String colorSeleccionadoShared = getPref(getApplicationContext().getString(R.string.colorSeleccionado), getApplicationContext());
+                    Toast.makeText(getApplicationContext(), "valor seleccionado->"+colorSeleccionadoShared, Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        
+        
         fabBorrar.setOnClickListener(v -> {
             borrar=true;
             pintar=false;
+            putPref(getString(R.string.seleccionado), getString(R.string.borrar), getApplicationContext());
+            String valorSeleccionado = getPref(getApplicationContext().getString(R.string.seleccionado), getApplicationContext());
+            Toast.makeText(this, "valor seleccionado->"+valorSeleccionado, Toast.LENGTH_SHORT).show();
         });
 
         fabPintar.setOnClickListener(v -> {
             pintar=true;
             borrar=false;
+            putPref(getString(R.string.seleccionado), getString(R.string.pintar), getApplicationContext());
+            String valorSeleccionado = getPref(getApplicationContext().getString(R.string.seleccionado), getApplicationContext());
+            Toast.makeText(this, "valor seleccionado->"+valorSeleccionado, Toast.LENGTH_SHORT).show();
+
         });
 
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("seleccionado", 0); // 0 - for private mode
+
+
+    }
+
+    private void pintarBorrar(View v, MotionEvent event) {
+        Context ctx = getApplicationContext();
+
+        String valorSeleccionado = getPref(ctx.getString(R.string.seleccionado), ctx);
+
+        //en caso de que se haya seleccionado el boton de pintar, se pintara del color seleccionado
+        //anteriormente en la paleta
+        if (valorSeleccionado.equalsIgnoreCase(ctx.getString(R.string.pintar))) {
+            String colorSeleccionadoShared = getPref(ctx.getString(R.string.colorSeleccionado), ctx);
+            Toast.makeText(getApplicationContext(), "color seleccionado grid->" + colorSeleccionadoShared, Toast.LENGTH_SHORT).show();
+            GridView parent=(GridView)v;
+            View vistaHija= parent.getChildAt(Integer.parseInt(getPref(getApplicationContext().getString(R.string.lineaSeleccionada), getApplicationContext())));
+            LinearLayout lnHija=(LinearLayout)vistaHija;
+            for(int i=0;i<lnHija.getChildCount();i++){
+                lnHija.getChildAt(i).getTag();
+
+
+                if(event.getX()==lnHija.getX()&&lnHija.getY()==event.getY()){
+
+                }
+            }
+            View vistaNieta= lnHija.getChildAt(0);
+            Toast.makeText(getApplicationContext(), "casilla seleccionada->"+getPref(getApplicationContext().getString(R.string.cuadradoSeleccionado), getApplicationContext()), Toast.LENGTH_SHORT).show();
+
+            vistaNieta.setBackgroundColor(Integer.parseInt(colorSeleccionadoShared));
+        } else if (valorSeleccionado.equalsIgnoreCase(ctx.getString(R.string.borrar))) {
+
+        }
 
     }
 
@@ -54,5 +153,15 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+    }
+    public static String getPref(String key, Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(key, null);
+    }
+    public static void putPref(String key, String value, Context context) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(key, value);
+        editor.apply();
     }
 }
